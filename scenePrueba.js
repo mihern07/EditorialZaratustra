b55b1cd3cc09a2b458494d8c8cbe884b59896d9c
mihern07 <mihern07@ucm.es>
2020-12-19 17:05:48 +0100

    Añadidos los cambios de Sergio y Miguel

--- scenePrueba.js
+++ scenePrueba.js
@@ -1,6 +1,5 @@
 import Clock from "./clock.js";
 import DeskBell from "./deskbell.js";
-import Character from "./character.js";
 import Inkwell from "./inkwell.js";
 import Board from "./board.js";
 import Pen from "./pen.js";
@@ -13,64 +12,6 @@ export default class Game extends Phaser.Scene {
 
   constructor() {
     super({ key: "main" });
-  }
-  preload() { //Carga de sprites
-    this.load.image("background", "sprites/background.png") //Fondo = background
-    this.load.image("foreground", "sprites/foreground.png") //Mesa = foreground
-    this.load.image("book", "sprites/librocerrado.png"); //Libro cerrado = book
-    this.load.image("book2", "sprites/libroabierto.png"); //libro abierto = book2
-    this.load.image("clock", "sprites/clockprototype.png"); //Reloj = clock
-    this.load.image("manecilla", "sprites/manecilla.png"); //Manecilla = manecilla
-    this.load.image("box", "sprites/dialogueboxv2.0.png"); //Bocadillo = box
-    this.load.image("character", "sprites/personaje.png"); //personaje = character
-    this.load.image("deskBellPressed", "sprites/timbrepulsado.png"); //timbre pulsado = deskBellPressed
-    this.load.spritesheet("deskBellSP", "sprites/timbresheet.png", { frameWidth: 385, frameHeight: 356 }); //timbre = deskBellSP
-    this.load.image("tinteroV", "sprites/tintero.png"); //tintero verde = tinteroV
-    this.load.image("tinteroR", "sprites/tinteroRojo.png"); // tintero rojo = tinteroR
-    this.load.image("document", "sprites/documento.png");
-    this.load.image("pen", "sprites/pen.png");
-    this.load.image("penV", "sprites/penGreen.png");
-    this.load.image("penR", "sprites/penRed.png");
-    this.load.image("board", "sprites/board.png"); // Board
-    this.load.image("postIt", "sprites/postitBlanco.png"); // PostIt
-    this.load.image("calendarOriginal", "sprites/calendar31_04.png");
-    this.load.image("paperParticle", "sprites/paperParticle.png");
-    this.load.image("bodyguard", "sprites/guardaespaldas.png"); //Guardaespaldas = bodyguard
-
-    //Sprites de los diferentes libros
-    this.load.image("libroCHistórico", "sprites/libro_cerrado_historia.png")
-    this.load.image("libroAHistórico", "sprites/libro_abierto_historia.png")
-    this.load.image("libroCAcadémico", "sprites/libro_cerrado_academico.png");
-    this.load.image("libroAAcadémico", "sprites/libro_abierto_academico.png");
-    this.load.image("libroCAventura", "sprites/libro_cerrado_aventura.png");
-    this.load.image("libroAAventura", "sprites/libro_abierto_aventura.png");
-    this.load.image("libroCComedia", "sprites/libro_cerrado_comedia.png");
-    this.load.image("libroAComedia", "sprites/libro_abierto_comedia.png");
-    this.load.image("libroCDrama", "sprites/libro_cerrado_drama.png");
-    this.load.image("libroADrama", "sprites/libro_abierto_drama.png");
-    this.load.image("libroCFantasía", "sprites/libro_cerrado_fantasia.png");
-    this.load.image("libroAFantasía", "sprites/libro_abierto_fantasia.png");
-    this.load.image("libroCFicción", "sprites/libro_cerrado_ficcion.png");
-    this.load.image("libroAFicción", "sprites/libro_abierto_ficcion.png");
-    this.load.image("libroCPolicíaco", "sprites/libro_cerrado_policiaco.png");
-    this.load.image("libroAPolicíaco", "sprites/libro_abierto_policiaco.png");
-    this.load.image("libroCRomance", "sprites/libro_cerrado_romance.png");
-    this.load.image("libroARomance", "sprites/libro_abierto_romance.png");
-    this.load.image("libroCSuspense", "sprites/libro_cerrado_suspense.png");
-    this.load.image("libroASuspense", "sprites/libro_abierto_suspense.png");
-
-    this.load.text("jefe", "dialogue/jefe.txt");
-    this.load.text("ninio", "dialogue/ninio.txt");
-    this.load.text("tendencias", "dialogue/personaje_tendencias.txt");
-    this.load.text("correos", "dialogue/correos.txt");
-    this.load.text("correosFalso", "dialogue/correos.txt");
-    this.load.text("mujerDelJefe", "dialogue/mujer_del_jefe.txt");
-    this.load.text("mujerdelJefeFalsa", "dialogue/mujer_del_jefe.txt");
-    this.load.text("sobornador", "dialogue/sobornador.txt");
-    this.load.text("vagabundo", "dialogue/vagabundo.txt");
-    this.load.text("dialogoBase", "dialogue/dialogoBase.txt");
-
-    this.load.audio("deskbellSound", "sounds/deskbell.wav"); //Audio timbre
   }
 
 
@@ -90,11 +31,6 @@ export default class Game extends Phaser.Scene {
     this.clock = new Clock(this, 750, 55, "clock", "manecilla"); //Inicializa reloj
     this.clock.start(this.handleTimeFinished.bind(this), '180000');
 
-    //Personaje
-    let archivoDialogo = this.cache.text.get("ninio");
-    archivoDialogo = archivoDialogo.split("\n");
-    this.chara = new Character(this, 955, 380, "character", archivoDialogo, "box", "book", "book2", "document"); //Inicializa personaje
-
     //FOREGROUND(MESA)
     this.fg = this.add.sprite(550, 392, "foreground");
 
@@ -103,9 +39,9 @@ export default class Game extends Phaser.Scene {
 
     //TINTEROS
 
-    this.tinteroVerde = new Inkwell(this, 200, 600, "tinteroV"); //Inicializa tintero verde
+    this.tinteroVerde = new Inkwell(this, 180, 670, "tinteroV"); //Inicializa tintero verde
 
-    this.tinteroRojo = new Inkwell(this, 950, 600, "tinteroR"); //Inicializa tintero rojo
+    this.tinteroRojo = new Inkwell(this, 925, 670, "tinteroR"); //Inicializa tintero rojo
 
     //CALENDARIO
     this.calendar = this.add.sprite(100, 300, "calendarOriginal");
@@ -152,9 +88,14 @@ export default class Game extends Phaser.Scene {
       numPagsMal: [1]
     }
 
+    //En algun punto seran constantes
+    let numCorrects = 10; // Número mínimo de libros correctos (7 necesarios + 3 de margen)
+    let minBooks= 17; // Número mínimo de libros entre los que se encuentran los anteriores
+
     this.events = new Events(this, 955, 380, "character", dialogoJefe, dialogoNinio, dialogoTendencias,
       dialogoCorreos, dialogoCorreosFalso, dialogoMujerDelJefe, dialogoMujerDelJefeFalsa,
-      dialogoSobornador, dialogoVagabundo, dialogoBase, "box", "book", "book2", "document", this.bookInfo);
+      dialogoSobornador, dialogoVagabundo, dialogoBase, "box", "book", "book2", "document", this.bookInfo,
+      numCorrects, minBooks);
 
     //DESKBELL
     this.bellSound = this.sound.add("deskbellSound");
@@ -174,12 +115,17 @@ export default class Game extends Phaser.Scene {
   }
 
   handleTimeFinished() {
-    this.scene.start('level2', this.gameManager);
+    if(this.gameManager.isCleared(1500))
+      this.scene.start('victoryScene', this.gameManager)
+    else
+      this.scene.start('gameOverScene', this.gameManager);
   }
 
 
 
   update(time, delta) {
+    this.events.update(); // No preUpdate porque no existe si hereda de GameObject
+
     if (this.physics.overlap(this.pen, this.tinteroRojo)) { //Overlap Rojo
       console.log("Hay solapeR");
       this.pen.setRed();
@@ -193,7 +139,7 @@ export default class Game extends Phaser.Scene {
     this.pen.changeColor();
 
     this.pen.PenR.on("pointerup", pointer => {
-      if (this.physics.overlap(this.pen, this.chara.document)) { //Overlap Documento Pluma Roja
+      if (this.physics.overlap(this.pen, this.events.chara.document)) { //Overlap Documento Pluma Roja
         this.events.DenyChar();
         this.pen.setNormal();
         console.log("Hay solapeD Deny");
@@ -201,7 +147,7 @@ export default class Game extends Phaser.Scene {
     })
 
     this.pen.PenV.on("pointerup", pointer => {
-      if (this.physics.overlap(this.pen, this.chara.document)) { //Overlap Documento Pluma Verde
+      if (this.physics.overlap(this.pen, this.events.chara.document)) { //Overlap Documento Pluma Verde
         this.events.AcceptChar();
         this.pen.setNormal();
         console.log("Hay solapeD Accept");
