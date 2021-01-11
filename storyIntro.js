750860eb394b3cbdebabf391e85e07897b6a601c
mihern07 <mihern07@ucm.es>
2021-01-11 10:39:50 +0100

    Actualización pantalla de intro

--- storyIntro.js
+++ storyIntro.js
@@ -5,21 +5,24 @@ export default class StoryIntro extends Phaser.Scene{
     preload(){
         this.load.text("intro", "dialogue/storyIntro.txt");
         this.load.image("street", "sprites/street.jpg");
+        this.load.image("fondo", "sprites/negro_semitransparente.png");
     }
 
     create(){
         this.background = this.add.sprite(550, 400, "street");
         this.background.setInteractive();
+        this.fondo = this.add.sprite(550, 400, "fondo");
+        this.fondo.setScale(.5);
         this.dialogoAPartir = this.cache.text.get("intro");
         this.dialogoAPartir = this.dialogoAPartir.split("\n");
 
         this.i = 0;
-        this.j = 2;
+        this.j = 4;
         this.text = this.dialogoAPartir.slice(this.i,this.j);
-        this.i = this.i + 2;
-        this.j = this.j + 2;
+        this.i = this.i + 4;
+        this.j = this.j + 4;
 
-        this.showText = this.add.text(200, 300, this.text);
+        this.showText = this.add.text(200, 350, this.text);
 
 
 
@@ -30,9 +33,9 @@ export default class StoryIntro extends Phaser.Scene{
                 if (this.text.length == 0){
                     this.scene.switch("main");
                 }
-                this.showText = this.add.text(200, 300, this.text);
-                this.i = this.i + 2;
-                this.j = this.j + 2;
+                this.showText = this.add.text(200, 350, this.text);
+                this.i = this.i + 4;
+                this.j = this.j + 4;
             }
           });
     }
